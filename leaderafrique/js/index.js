8e00b9f2a76c7c34e3240e596987d1dda9bc070a
Jephthah Essel <157823716+jevisexcell024@users.noreply.github.com>
2024-08-07 00:26:23 +0000

    Add files via upload

---
+++ leaderafrique/js/index.js
@@ -0,0 +1,18 @@
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+  }
+  function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+  }
+
+
+  function show(){
+    const button = document.querySelector('.hide')
+    .hide.style.display = 'flex'
+  }
+  function hide(){
+    const button = document.querySelector('.hide')
+    .hide.style.display = 'none'
+  }
